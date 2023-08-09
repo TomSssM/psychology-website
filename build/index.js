3af743be0162cdfeea4af4b2802199283756eda9
TomSssM <ilyashome3@gmail.com>
2023-08-09 18:57:44 +0300

    deploy build

--- build/index.js
+++ build/index.js
@@ -1,1 +1,22 @@
+// TODO: interactive accordions
+/*
+class Accordion extends View {
+  constructor(options) {
+    super(options);
+    this.liveBindTo('title', 'click', this.onTitleClick);
+  }
+
+  onTitleClick(title) {
+    this.findOn(title, 'item').toggleMod('expanded');
+  }
+}
+
+Accordion.init();
+*/ // TODO: interactive sliders
+// TODO: interactive #contact-button that scrolls to #contact-section and make the button display none as well
+// TODO: the rest of todos from
+// *   code
+// *   TODO folder
+// *   Keep
+// TODO: you may want to backup this website somewhere
 "use strict";
