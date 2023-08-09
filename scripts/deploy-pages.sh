9c9d7387a88b637424a25e9b11b0c2d9c254b4cc
TomSssM <ilyashome3@gmail.com>
2023-08-09 20:28:59 +0300

    create basic deploy scripts

---
+++ scripts/deploy-pages.sh
@@ -0,0 +1,16 @@
+mkdir ../deploy
+cp ./index.html ../deploy
+npm run build
+cp -r ./build ../deploy/build
+git add -A
+git stash
+git checkout gh-pages
+rm -rf index.html build
+cp ../deploy/index.html ./index.html
+cp -r ../deploy/build ./build
+git add -A
+git commit -m 'deploy build'
+git push
+git checkout master
+git stash pop
+rm -rf ../deploy
---
+++ scripts/deploy.sh
@@ -0,0 +1,1 @@
+bash ./scripts/deploy-pages.sh
