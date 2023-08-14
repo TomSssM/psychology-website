19d486518e791087ae8ffda65acf127d93330cd4
TomSssM <ilyashome3@gmail.com>
2023-08-14 21:51:10 +0300

    fix deploy script further

--- scripts/deploy-pages.sh
+++ scripts/deploy-pages.sh
@@ -5,10 +5,12 @@ mkdir ../deploy
 cp ./index.html ../deploy
 npm run build
 cp -r ./build ../deploy/build
+cp -r ./assets ../deploy/assets
 git checkout gh-pages
-rm -rf index.html build
+rm -rf index.html build assets
 cp ../deploy/index.html ./index.html
 cp -r ../deploy/build ./build
+cp -r ../deploy/assets ./assets
 git add -A
 git commit -m 'deploy build'
 git push
