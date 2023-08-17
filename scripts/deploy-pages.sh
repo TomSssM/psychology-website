afe7289b4621dbd9824be0f9819058fe4f5d911e
TomSssM <ilyashome3@gmail.com>
2023-08-17 17:31:28 +0300

    update deploy script

--- scripts/deploy-pages.sh
+++ scripts/deploy-pages.sh
@@ -3,12 +3,14 @@ git stash
 rm -rf ./build
 mkdir ../deploy
 cp ./index.html ../deploy
+cp ./favicon.ico ../deploy
 npm run build
 cp -r ./build ../deploy/build
 cp -r ./assets ../deploy/assets
 git checkout gh-pages
-rm -rf index.html build assets
+rm -rf index.html favicon.ico build assets
 cp ../deploy/index.html ./index.html
+cp ../deploy/favicon.ico ./favicon.ico
 cp -r ../deploy/build ./build
 cp -r ../deploy/assets ./assets
 git add -A
